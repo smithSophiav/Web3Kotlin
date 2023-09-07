29b8943a6e76b64f7638885d21aca58d7245e716
smithSophiav <smith19850306@gmail.com>
2023-09-07 15:44:43 +0800

    add files

---
+++ app/src/main/java/com/smithSophiav/web3kotlindemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smithSophiav.web3kotlindemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
